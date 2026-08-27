d6a7a4fc2ca1fbf64bc0accf2360aa7081627c75
Andres Lenis <andreslenis5090@gmail.com>
2026-08-27 18:14:28 -0500

    Primer commit: API sleep-tracker-lab

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+app = FastAPI()
+
+class UsuarioCreate(BaseModel):
+    nombre: str
+    apellido: str
+    ocupacion: str
+    meta_sueno: float
+    email: str
+    contrasena: str
+
+@app.get("/")
+def root():
+    return {"mensaje": "API de sleep-tracker-lab funcionando"}
+
+@app.post("/usuarios")
+def crear_usuario(usuario: UsuarioCreate):
+    response = supabase.table("usuario").insert(usuario.model_dump()).execute()
+    return response.data
+
+@app.get("/registros")
+def listar_registros():
+    response = supabase.table("registro_sueno").select("*, usuario(nombre, apellido)").execute()
+    return response.data
